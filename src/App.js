fac502281d21ab646d075651e08815dcc0508675
raourghi <aourghinew@gmail.com>
2024-03-05 16:00:58 +0100

    crypto-tracker

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CoinApp from './components/CoinApp';
+
+function App() {
+  return (
+    <>
+      <CoinApp />
+    </>
+  );
+}
+
+export default App;
